f0f39b1e29509f9577d22b5a0432d286a86f5aef
JuliusR <>
2021-01-04 21:28:51 +0100

    fix shebang lines

--- jdbc_pg_backup/jdbc_pg_backup.sh
+++ jdbc_pg_backup/jdbc_pg_backup.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env -S bash -euxET -o pipefail -O inherit_errexit
+#!/bin/bash
+set -ex
 
 java -cp "target/jdbc_pg_backup-1.0-SNAPSHOT.jar:target/lib/*" jdbcpgbackup.JdbcPgBackup $@
--- postgres/docker-entrypoint-initdb.d/createuser.sh
+++ postgres/docker-entrypoint-initdb.d/createuser.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env -S bash -euxET -o pipefail -O inherit_errexit
+#!/bin/bash
+set -ex
 
 if [ -z "$NABBLE_EXPORT_PROCESSOR_SCHEMA" ]; then
     >&2 echo "ERROR: found empty value of NABBLE_EXPORT_PROCESSOR_SCHEMA"
